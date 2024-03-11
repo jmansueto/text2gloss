a8e8983c203cad37fb7c5894d674a79fa8e13760
jmansueto <jennam141@gmail.com>
2024-03-11 03:01:22 +0000

    tiny transformer

---
+++ code/models/transformer_tiny/train.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# to make script executable, run chmod +x train.sh 
+# NOTE TO SELF: MAKE PATHS ABSOLUTE
+DATA='/home/jennamansueto/text2gloss/code/data/aslg_pc12/clean/' 
+SRC_LANG='en' #Source Language
+TGT_LANG='asl' #Target Language
+LOG_ROOT_DIR='/home/jennamansueto/text2gloss/code/models/logs'
+LOG_DIR="${LOG_ROOT_DIR}/transformer_tiny"
+
+TOKENIZER_TYPE='moses' 
+BPE_TYPE='subword_nmt' 
+BPE_CODES="data/alsg_pc12/bpe_codes.txt" 
+
+EPOCH=30 #Number of epochs
+OPTIMIZER='adam' 
+SCHEDULER='inverse_sqrt' #Learning rate growth planner
+LOSS='label_smoothed_cross_entropy' #Loss metric
+ARCHITECTURE='transformer_tiny' #Basic transformer
+LEARNING_RATE=5e-4 
+MAX_TOKENS=2048 #Maximum number of tokens in a sentence
+
+SAVE_DIR='/home/jennamansueto/text2gloss/code/models/transformer_tiny/checkpoints/'
+
+#--warmup-updates: How many learning rate warmup steps to use
+CUDA_VISIBLE_DEVICES=0,1 fairseq-train $DATA \
+        --source-lang $SRC_LANG --target-lang $TGT_LANG \
+        --bpe $BPE_TYPE --bpe-codes=$BPE_CODES \
+        --max-epoch $EPOCH \
+        --max-tokens $MAX_TOKENS \
+        --optimizer $OPTIMIZER \
+        --lr-scheduler $SCHEDULER --warmup-updates 4000 \
+        --dropout 0.1 --weight-decay 0.0 \
+        --arch $ARCHITECTURE \
+        --lr $LEARNING_RATE \
+        --criterion $LOSS \
+        --share-decoder-input-output-embed \
+        --encoder-learned-pos \
+        --update-freq 8 \
+        --save-interval-updates 10000 \
+        --validate-interval-updates 10000 \
+        --save-dir $SAVE_DIR \
+        --tensorboard-logdir $LOG_DIR \
+        --patience 5 \
